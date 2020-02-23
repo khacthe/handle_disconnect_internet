abd38f8d40292ab0ceb165e332990fe2c64cf94b
khacthe <khacthe.python@gmail.com>
2020-02-24 04:46:11 +0700

    Render page example

--- README.md
+++ README.md
@@ -129,3 +129,35 @@ export default function (ComposedComponent) {
 
 ```
 
+### Render page
+- Ta render page từ thông qua App.js
+
+```
+import React, { Component } from 'react';
+import './App.css';
+import images from './helper/images'
+import Detect from './hocs/Detect';
+
+class App extends Component {
+  renderImage() {
+    return (
+      <div className='image-list'>
+        {images.map(data => <img src={data.image} alt='random' key={data.id} className="image" />)}
+      </div>
+    )
+  }
+  render() {
+    return (
+      <div className="App">
+        <p className="page-title">Handle Disconnect Internet</p>
+        {this.renderImage()}
+      </div>
+    );
+  }
+}
+
+export default Detect(App);
+
+```
+
+- Mở page trên trình duyệt, tiến hành thực hiện disconect internet để kiểm tra kết quả.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import images from './helper/images'
+import Detect from './hocs/Detect';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  renderImage() {
+    return (
+      <div className='image-list'>
+        {images.map(data => <img src={data.image} alt='random' key={data.id} className="image" />)}
+      </div>
+    )
+  }
+  render() {
+    return (
+      <div className="App">
+        <p className="page-title">Handle Disconnect Internet</p>
+        {this.renderImage()}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Detect(App);
